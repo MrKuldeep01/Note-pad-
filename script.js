b624c2a6518006dbafa364eece4492aa8317c923
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2023-10-17 13:40:42 +0530

    update code

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-themeBtn=document.getElementById('themeToggle');
-textBox=document.getElementById('textArea');
-themeBtn.addEventListener('click',()=>{
-    textBox.classList.toggle('darkTheme');
-    
-    
+let themeBtn = document.getElementById("themeToggle");
+let textBox = document.getElementById("textArea");
+const saveBtn = document.getElementById("saveBtn");
+const deleteBtn = document.getElementById("closeBtn");
+const editBtn = document.getElementById("editBtn");
+
+themeBtn.addEventListener("click", () => {
+  textBox.classList.toggle("darkTheme");
 });
+
+function saveNotes() {
+  let textBox = document.getElementById("textArea").value;
+  const userFile = prompt("enter file");
+  localStorage.setItem(userFile, textBox);
+
+  document.getElementById("textArea").value = "";
+}
+
+saveBtn.addEventListener("click", saveNotes);
